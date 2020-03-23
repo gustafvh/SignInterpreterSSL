56e2bb8f780425f7bb0a0640ad8cf5fcb80285c4
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2020-03-23 13:41:54 +0100

    new format and functionstructure

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import pandas as pd
-import numpy as np
-import os
-import keras
-import matplotlib.pyplot as plt
-
 from PIL import Image
 import numpy as np
 from skimage import transform
 
-from keras.layers import Dense,GlobalAveragePooling2D
+from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications import MobileNet
 from keras.applications.mobilenet import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
@@ -17,79 +11,84 @@ from keras.models import model_from_json
 
 import cv2
 
-
 #### Variables ######
 
 TRAINING_DATA_SET_PATH = './data/asl-alphabet/asl_alphabet_train/asl_alphabet_train'
+EPOCHS = 5
+BATCH_SIZE = 32
+IMAGE_HEIGHT = 128
+IMAGE_WIDTH = 128
 
 
+####################
 
 def getPreTrainedModel():
-    base_model=MobileNet(weights='imagenet',include_top=False) #imports the mobilenet model and discards the last 1000 neuron layer.
+    pre_trained_base_model = MobileNet(weights='imagenet',
+                                       include_top=False)  # Remove last neuron layer (that contains 1000 neurons)
 
-    x=base_model.output
-    x=GlobalAveragePooling2D()(x)
-    x=Dense(1024,activation='relu')(x) #we add dense layers so that the model can learn more complex functions and classify for better results.
-    x=Dense(1024,activation='relu')(x) #dense layer 2
-    x=Dense(512,activation='relu')(x) #dense layer 3
-    preds=Dense(3,activation='softmax')(x) #final layer with softmax activation
-    model=Model(inputs=base_model.input,outputs=preds)
-    return model
+    my_model = pre_trained_base_model.output
+    my_model = GlobalAveragePooling2D()(my_model)
+    my_model = Dense(1000, activation='relu')(
+        my_model)  # Add layer. Layer #1: 1024 nodes, and rectified linear activation function
+    my_model = Dense(1000, activation='relu')(my_model)  # Layer #2
+    my_model = Dense(500, activation='relu')(my_model)
+    final_layer = Dense(3, activation='softmax')(my_model) # 3 because it could be A,B or C
+    final_model = Model(inputs=pre_trained_base_model.input, outputs=final_layer)
+    return final_model
+
 
 def freezeLayers(numOfLayers, model):
-    for layer in model.layers[:numOfLayers]:
-        layer.trainable=False
-    for layer in model.layers[numOfLayers:]:
-        layer.trainable=True
+    for single_layer in model.layers[:numOfLayers]:
+        single_layer.trainable = False
+    for single_layer in model.layers[numOfLayers:]:
+        single_layer.trainable = True
 
     return model
 
-def fitModel(model):
-    train_datagen=ImageDataGenerator(preprocessing_function=preprocess_input) #included in our dependencies
 
-    train_generator=train_datagen.flow_from_directory(TRAINING_DATA_SET_PATH, # this is where you specify the path to the main data folder
-                                                      target_size=(224,224),
-                                                      color_mode='rgb',
-                                                      batch_size=32,
-                                                      class_mode='categorical',
-                                                      shuffle=True)
+def fitModelWithTrainingData(model):
+    datagen = ImageDataGenerator(preprocessing_function=preprocess_input)  # For training data
 
+    image_generator = datagen.flow_from_directory(TRAINING_DATA_SET_PATH,
+                                                  target_size=(IMAGE_HEIGHT, IMAGE_WIDTH),
+                                                  color_mode='rgb',
+                                                  batch_size=BATCH_SIZE,
+                                                  class_mode='categorical',
+                                                  shuffle=True)
 
-    model.compile(optimizer='Adam',loss='categorical_crossentropy',metrics=['accuracy'])
-    # Adam optimizer
-    # loss function will be categorical cross entropy
-    # evaluation metric will be accuracy
+    model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-    step_size_train=train_generator.n//train_generator.batch_size
-    model.fit_generator(generator=train_generator,
-                        steps_per_epoch=step_size_train,
-                        epochs=5)
+    step_size_training = image_generator.n // image_generator.batch_size  # Balance here to find optimal step_size
+    model.fit_generator(generator=image_generator,
+                        steps_per_epoch=step_size_training,
+                        epochs=EPOCHS)
     return model
+
 
 def saveModelToJson(model):
-    # serialize model to JSON
     model_json = model.to_json()
     with open("./output/model.json", "w") as json_file:
         json_file.write(model_json)
-    # serialize weights to HDF5
+
     model.save_weights("./output/model.h5")
     print("Saved model to disk")
 
+
 def loadModelfromJson(modelPath, weightPath):
-    # load json and create model
     json_file = open(modelPath, 'r')
     loaded_model_json = json_file.read()
     json_file.close()
     loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
+
     loaded_model.load_weights(weightPath)
     print("Loaded model from disk")
     return loaded_model
 
+
 def loadSingleImage(filename):
     np_image = Image.open(filename)
-    np_image = np.array(np_image).astype('float32')/255
-    np_image = transform.resize(np_image, (256, 256, 3))
+    np_image = np.array(np_image).astype('float32') / 255
+    np_image = transform.resize(np_image, (IMAGE_HEIGHT, IMAGE_WIDTH, 3))
     np_image = np.expand_dims(np_image, axis=0)
     return np_image
 
@@ -97,7 +96,7 @@ def loadSingleImage(filename):
 def trainNetwork():
     preTrainedModel = getPreTrainedModel()
     trimmedModel = freezeLayers(20, preTrainedModel)
-    finalModel = fitModel(trimmedModel)
+    finalModel = fitModelWithTrainingData(trimmedModel)
     saveModelToJson(finalModel)
     return finalModel
 
@@ -119,10 +118,11 @@ def showImageCV(preds):
     cv2.imshow("Image", output)
     cv2.waitKey(0)
 
+
 def mainPipeline():
-    #finalModel = trainNetwork()
+    # finalModel = trainNetwork()
     finalModel = loadModelfromJson('./output/model.json', './output/model.h5')
-    image = loadSingleImage('./data/asl-alphabet/asl_alphabet_train/asl_alphabet_train/C/C1.jpg')
+    image = loadSingleImage('./data/asl-alphabet/asl_alphabet_train/asl_alphabet_train/B/B1.jpg')
     predictions = finalModel.predict(image)
     # Predictions contains a 2D array where the index of the
     # images sent in (so for a single image its only a
@@ -130,7 +130,7 @@ def mainPipeline():
     # each class A,B,C
     print(predictions)
     print(predictions.astype('int'))
-    showImageCV(predictions)
+    #showImageCV(predictions)
 
 
 mainPipeline()
