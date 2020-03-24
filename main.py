bfb38e7d4bbcb91a2adec66dec3f6cf8907fba03
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2020-03-24 16:10:49 +0100

    added getTopPredictions, works at 54% now

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PIL import Image
 import numpy as np
 import pandas as pd
+import json
 from skimage import transform
+from matplotlib import pyplot as plt
 
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications import MobileNet
@@ -9,6 +11,9 @@ from keras.applications.mobilenet import preprocess_input
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Model
 from keras.models import model_from_json
+
+from keras.utils import plot_model
+
 
 import cv2
 
@@ -18,7 +23,7 @@ from keras.datasets import mnist
 
 TRAINING_DATA_SET_PATH = './data/asl-alphabet-medium/asl_alphabet_train'
 VALIDATION_DATA_SET_PATH = './data/asl-alphabet-medium/asl_alphabet_test'
-EPOCHS = 5
+EPOCHS = 5 #5 gives 54% accuracy
 BATCH_SIZE = 32
 IMAGE_HEIGHT = 224
 IMAGE_WIDTH = 224
@@ -33,10 +38,10 @@ def getPreTrainedModel():
 
     my_model = pre_trained_base_model.output
     my_model = GlobalAveragePooling2D()(my_model)
-    my_model = Dense(1024, activation='relu')(
+    my_model = Dense(1000, activation='relu')(
         my_model)  # Add layer. Layer #1: 1000 nodes, and rectified linear activation function
-    my_model = Dense(1024, activation='relu')(my_model)  # Layer #2
-    my_model = Dense(512, activation='relu')(my_model)
+    my_model = Dense(1000, activation='relu')(my_model)  # Layer #2
+    my_model = Dense(500, activation='relu')(my_model)
     final_layer = Dense(DATASET_CATEGORIES, activation='softmax')(my_model)  # DATASET_CATEGORIES because it could be A,B,C etc.
     final_model = Model(inputs=pre_trained_base_model.input, outputs=final_layer)
     return final_model
@@ -74,11 +79,15 @@ def fitModel(model):
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     step_size_training = train_generator.n // train_generator.batch_size  # Balance here to find optimal step_size
-    model.fit_generator(generator=train_generator,
+    history = model.fit_generator(generator=train_generator,
                         steps_per_epoch=step_size_training,
                         epochs=EPOCHS,
                         validation_data=validation_generator,
                         validation_steps=validation_generator.n // BATCH_SIZE)
+
+    # # Save training history as file. For ex. loss and accuracy in each epoch
+    # with open('./output/history.json', 'w') as file:
+    #     json.dump(history.history, file)
 
     return model
 
@@ -90,6 +99,28 @@ def trainNetwork():
     saveModelToJson(finalModel)
     return finalModel
 
+
+def plotEpochPerformance(history):
+    # Plot training & validation accuracy values
+    plt.plot(history.history['acc'])
+    plt.plot(history.history['val_acc'])
+    plt.title('Model accuracy')
+    plt.ylabel('Accuracy')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+    # Plot training & validation loss values
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('Model loss')
+    plt.ylabel('Loss')
+    plt.xlabel('Epoch')
+    plt.legend(['Train', 'Test'], loc='upper left')
+    plt.show()
+
+    # This will plot a graph of the model and save it to a file:
+    #plot_model(model, to_file='model.png')
 
 def saveModelToJson(model):
     model_json = model.to_json()
@@ -137,79 +168,88 @@ def showImageCV(preds, imagePath, letter):
 
 
 def getTopPredictions(preds):
-    # top_preds = preds.argmax(axis=1)[0]
+    #top_preds = preds.argmax(axis=1)[0]
 
+    predsDict = {
+        'A': 0, 'B': 0, 'C': 0, 'D': 0, 'del': 0, 'E': 0, 'F': 0, 'G': 0,
+        'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'nothing': 0,
+        'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'space': 0, 'T': 0,
+        'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0
+    }
+    #map preds index with probability to correct letter from dictonary
+    for i, key in enumerate(predsDict, start=0):
+        predsDict[key] = preds[i]
+
+
+    # sort by dictonary value and returns as list
+    all_preds = sorted(predsDict.items(), reverse=True, key=lambda x: x[1])
+
+    top_preds = [all_preds[0], all_preds[1], all_preds[2]]
+    # for i, key in enumerate(all_preds, start=0):
+    #     top_preds.append(
+
+    #print(all_preds.keys()[0])
     # Convert from float into percentages
-    top_preds = pd.DataFrame(preds / float(np.sum(preds))).applymap(lambda preds: '{:.2%}'.format(preds)).values
+    #top_preds = pd.DataFrame(preds / float(np.sum(preds))).applymap(lambda preds: '{:.2%}'.format(preds)).values
+    #print(preds[0])
 
-    return top_preds
+    return top_preds, all_preds
 
 
 def evaluateModel(model):
-    test_datagen = ImageDataGenerator(rescale=1. / 255,
-                                      preprocessing_function=preprocess_input)
+    val_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
 
-    validation_generator = test_datagen.flow_from_directory(
+    validation_generator = val_datagen.flow_from_directory(
         VALIDATION_DATA_SET_PATH,
         target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
         batch_size=BATCH_SIZE,
         class_mode='categorical')
 
     model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    score = model.evaluate_generator(validation_generator, validation_generator.n / BATCH_SIZE, workers=12)
-
-    print("Loss: ", score[0], "Accuracy: ", score[1]*100, '%')
+    score = model.evaluate_generator(validation_generator,
+                                     validation_generator.n / BATCH_SIZE,
+                                     workers=12, use_multiprocessing=True, verbose=1)
+    loss, accuracy = score[0], score[1]
+    return loss, accuracy
 
 
 def mainPipeline():
     #finalModel = trainNetwork()
     finalModel = loadModelfromJson('./output/model.json', './output/model.h5')
 
-    letter = 'G'
-    #imagePath = './data/asl-alphabet/asl_alphabet_train/' + letter + '/' + letter + '575.jpg'
-    imagePath = './data/test-images/' + letter + '/' + letter + '.jpg'
+
+    letter = 'K'
+    imagePath = './data/asl-alphabet/asl_alphabet_train/' + letter + '/' + letter + '575.jpg'
+    #imagePath = './data/test-images/' + letter + '/' + letter + '.jpg'
     image = loadSingleImage(imagePath)
-    predictions = finalModel.predict(image)
 
-
-
-    # Predictions contains a 2D array where the index of the
-    # images sent in (so for a single image its only a
-    # prediction[0]) and in that array is the prediction score for
-    # each class A,B,C
-    # user a dataframe to format the numbers and then convert back to a numpy array.
-    print('*****************************************************')
-    print('Predictions that its [Class A, Class B, ....]')
-    # print(predictions)
-    print(getTopPredictions(predictions))
-    # print(predictions)
+    predictions = finalModel.predict(image,
+                                     workers=12,
+                                     use_multiprocessing=True)
 
 
     print('*****************************************************')
+    #print(predictions)
+    loss, accuracy = evaluateModel(finalModel)
+    print("Loss: ", loss, "Accuracy: ", accuracy*100, '%')
+
+    print('Input was:', letter)
+
+
+    top_three_preds, all_preds = getTopPredictions(predictions[0])
+    print(top_three_preds)
+    #evaluateModel(finalModel)
+    # print(predictions)
+
+
+    print('*****************************************************')
+    #print(finalModel.summary())
     #print(evaluateModel(finalModel))
 
-    showImageCV(predictions, imagePath, letter)
+    #showImageCV(predictions, imagePath, letter)
 
 
 mainPipeline()
 
-# (X_train, y_train), (X_test, y_test) = mnist.load_data()
-# print(np.shape(X_train))
-# print(type(X_train))
 
 
-
-# Current Model:
-# Found 2900 images belonging to 29 classes.
-# Found 29 images belonging to 29 classes.
-# Epoch 1/5
-# 90/90 [==============================] - 261s 3s/step - loss: 0.2957 - accuracy: 0.9344 - val_loss: 5.4062 - val_accuracy: 0.1034
-# Epoch 2/5
-# 90/90 [==============================] - 246s 3s/step - loss: 0.0931 - accuracy: 0.9784 - val_loss: 1.7850 - val_accuracy: 0.6207
-# Epoch 3/5
-# 90/90 [==============================] - 250s 3s/step - loss: 0.0260 - accuracy: 0.9934 - val_loss: 14.2982 - val_accuracy: 0.1724
-# Epoch 4/5
-# 90/90 [==============================] - 263s 3s/step - loss: 0.1490 - accuracy: 0.9718 - val_loss: 13.2542 - val_accuracy: 0.1034
-# Epoch 5/5
-# 90/90 [==============================] - 228s 3s/step - loss: 0.0223 - accuracy: 0.9972 - val_loss: 0.2101 - val_accuracy: 0.9655
-# Model saved as ./output/model.h5
