66b8a783b30bc24b749e754e3d2b8f5f08f39886
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2020-03-25 11:33:20 +0100

    small reformating

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PIL import Image
 import numpy as np
+
 # Weights are initiliased at random so this makes our model deterministic (same result each time)
-np.random.seed(123) # for reproducibility
+np.random.seed(123)  # for reproducibility
 import pandas as pd
 import json
 from skimage import transform
@@ -15,26 +16,44 @@ from keras.models import Model
 from keras.models import model_from_json
 from keras.callbacks import ModelCheckpoint
 
+# from kerastuner.tuners import RandomSearch
+# from kerastuner.engine.hyperparameters import HyperParameters
 
 from keras.utils import plot_model
 
-
 import cv2
 import os
-
 
 #### Variables ######
 
 TRAINING_DATA_SET_PATH = './data/asl-alphabet-medium/asl_alphabet_train'
 VALIDATION_DATA_SET_PATH = './data/asl-alphabet-medium/asl_alphabet_test'
-EPOCHS = 5 #5 gives 54% accuracy
+EPOCHS = 10  # 50-100 is a good range to try later
 BATCH_SIZE = 32
 IMAGE_HEIGHT = 224
 IMAGE_WIDTH = 224
 DATASET_CATEGORIES = 29
 
-
 ####################
+
+
+train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)  # For training data
+
+val_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+train_generator = train_datagen.flow_from_directory(TRAINING_DATA_SET_PATH,
+                                                    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+                                                    color_mode='rgb',
+                                                    batch_size=BATCH_SIZE,
+                                                    class_mode='categorical',
+                                                    shuffle=True)
+
+validation_generator = val_datagen.flow_from_directory(
+    VALIDATION_DATA_SET_PATH,
+    target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
+    batch_size=BATCH_SIZE,
+    class_mode='categorical')
+
 
 def getPreTrainedModel():
     pre_trained_base_model = MobileNet(weights='imagenet',
@@ -46,7 +65,8 @@ def getPreTrainedModel():
         my_model)  # Add layer. Layer #1: 1000 nodes, and rectified linear activation function
     my_model = Dense(1024, activation='relu')(my_model)  # Layer #2
     my_model = Dense(512, activation='relu')(my_model)
-    final_layer = Dense(DATASET_CATEGORIES, activation='softmax')(my_model)  # DATASET_CATEGORIES because it could be A,B,C etc.
+    final_layer = Dense(DATASET_CATEGORIES, activation='softmax')(
+        my_model)  # DATASET_CATEGORIES because it could be A,B,C etc.
     final_model = Model(inputs=pre_trained_base_model.input, outputs=final_layer)
     return final_model
 
@@ -61,25 +81,6 @@ def freezeLayers(numOfLayers, model):
 
 
 def fitModel(model):
-    train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)  # For training data
-
-    # this is the augmentation configuration we will use for testing:
-    # only rescaling
-    val_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
-
-    train_generator = train_datagen.flow_from_directory(TRAINING_DATA_SET_PATH,
-                                                        target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-                                                        color_mode='rgb',
-                                                        batch_size=BATCH_SIZE,
-                                                        class_mode='categorical',
-                                                        shuffle=True)
-
-    validation_generator = val_datagen.flow_from_directory(
-        VALIDATION_DATA_SET_PATH,
-        target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-        batch_size=BATCH_SIZE,
-        class_mode='categorical')
-
     model.compile(optimizer='Adam', loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
@@ -92,11 +93,11 @@ def fitModel(model):
 
     step_size_training = train_generator.n // train_generator.batch_size  # Balance here to find optimal step_size
     history = model.fit_generator(generator=train_generator,
-                        steps_per_epoch=step_size_training,
-                        epochs=EPOCHS,
-                        validation_data=validation_generator,
-                        validation_steps=validation_generator.n // BATCH_SIZE,
-                        callbacks=callbacks_list)
+                                  steps_per_epoch=step_size_training,
+                                  epochs=EPOCHS,
+                                  validation_data=validation_generator,
+                                  validation_steps=validation_generator.n // BATCH_SIZE,
+                                  callbacks=callbacks_list)
 
     # # Save training history as file. For ex. loss and accuracy in each epoch
     # with open('./output/history.json', 'w') as file:
@@ -133,7 +134,8 @@ def plotEpochPerformance(history):
     plt.show()
 
     # This will plot a graph of the model and save it to a file:
-    #plot_model(model, to_file='model.png')
+    # plot_model(model, to_file='model.png')
+
 
 def saveModelToJson(model):
     model_json = model.to_json()
@@ -181,7 +183,7 @@ def showImageCV(preds, imagePath, letter):
 
 
 def getTopPredictions(preds):
-    #top_preds = preds.argmax(axis=1)[0]
+    # top_preds = preds.argmax(axis=1)[0]
 
     predsDict = {
         'A': 0, 'B': 0, 'C': 0, 'D': 0, 'del': 0, 'E': 0, 'F': 0, 'G': 0,
@@ -189,10 +191,9 @@ def getTopPredictions(preds):
         'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'space': 0, 'T': 0,
         'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0
     }
-    #map preds index with probability to correct letter from dictonary
+    # map preds index with probability to correct letter from dictonary
     for i, key in enumerate(predsDict, start=0):
         predsDict[key] = preds[i]
-
 
     # sort by dictonary value and returns as list
     all_preds = sorted(predsDict.items(), reverse=True, key=lambda x: x[1])
@@ -201,22 +202,15 @@ def getTopPredictions(preds):
     # for i, key in enumerate(all_preds, start=0):
     #     top_preds.append(
 
-    #print(all_preds.keys()[0])
+    # print(all_preds.keys()[0])
     # Convert from float into percentages
-    #top_preds = pd.DataFrame(preds / float(np.sum(preds))).applymap(lambda preds: '{:.2%}'.format(preds)).values
-    #print(preds[0])
+    # top_preds = pd.DataFrame(preds / float(np.sum(preds))).applymap(lambda preds: '{:.2%}'.format(preds)).values
+    # print(preds[0])
 
     return top_preds, all_preds
 
 
 def evaluateModel(model):
-    val_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
-
-    validation_generator = val_datagen.flow_from_directory(
-        VALIDATION_DATA_SET_PATH,
-        target_size=(IMAGE_WIDTH, IMAGE_HEIGHT),
-        batch_size=BATCH_SIZE,
-        class_mode='categorical')
 
     model.compile(optimizer='Adam', loss='categorical_crossentropy',
                   metrics=['accuracy'])
@@ -230,41 +224,33 @@ def evaluateModel(model):
 
 def mainPipeline():
     finalModel = trainNetwork()
-    finalModel = loadModelfromJson('./output/model.json', './output/model-weights.best.hdf5')
-
+    #finalModel = loadModelfromJson('./output/model.json', './output/model.h5')
 
     letter = 'A'
     imagePath = './data/asl-alphabet/asl_alphabet_train/' + letter + '/' + letter + '575.jpg'
-    #imagePath = './data/test-images/' + letter + '/' + letter + '.jpg'
+    # imagePath = './data/test-images/' + letter + '/' + letter + '.jpg'
     image = loadSingleImage(imagePath)
     predictions = finalModel.predict(image,
                                      workers=12,
                                      use_multiprocessing=True)
 
-
     print('*****************************************************')
-    #print(predictions)
+    # print(predictions)
     loss, accuracy = evaluateModel(finalModel)
-    print("Loss: ", loss, "Accuracy: ", accuracy*100, '%')
+    print("Loss: ", loss, "Accuracy: ", accuracy * 100, '%')
     print('Input was:', letter)
     top_three_preds, all_preds = getTopPredictions(predictions[0])
     print(top_three_preds)
     # print(predictions)
 
-
     print('*****************************************************')
-    #print(finalModel.summary())
-    #print(evaluateModel(finalModel))
+    # print(finalModel.summary())
+    # print(evaluateModel(finalModel))
 
-    #showImageCV(predictions, imagePath, letter)
+    # showImageCV(predictions, imagePath, letter)
 
-    #Output updated training data structure in text-file
+    # Output updated training data structure in text-file
     os.system("tree --filelimit=20 > project-file-structure.txt")
 
 
-
-
 mainPipeline()
-
-
-
