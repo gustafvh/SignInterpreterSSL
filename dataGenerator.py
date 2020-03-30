53824c674d0eb0a58da19a9e2dc03e1fd810dd91
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2020-03-30 16:13:02 +0200

    add train/test split

--- dataGenerator.py
+++ dataGenerator.py
@@ -2,11 +2,12 @@
 import cv2
 import os
 
-# categories = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-#               'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
-#               'V', 'W', 'X', 'Y', 'Z', 'Å', 'Ä', 'Ö']
+categories = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+              'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+              'V', 'W', 'X', 'Y', 'Z', 'Å', 'Ä', 'Ö']
 
-categories = ['G']
+
+# categories = ['A']
 
 
 def createImagesFromClip(letter):
@@ -24,30 +25,34 @@ def createImagesFromClip(letter):
         live, frame = videoStream.read()
         everyOther = True
 
-        if live:  # Continue as long as video still has frames
+        if live and currentframe <= 600:  # Continue as long as video still has frames
 
-            try:
-                # creating a folder named data
-                dataFolder = 'data/SSL-dataset/train/' + letter
+            if currentframe <= 500: #Train/Test split
+                folder = 'train'
+            else:
+                folder = 'test'
+            try:  # Create a folder if it doesnt exists
+                dataFolder = 'data/SSL-dataset/' + folder + '/' + letter
                 if not os.path.exists(dataFolder):
                     os.makedirs(dataFolder)
 
-                # if not created then raise error
+
             except OSError:
                 pass
 
-            name = './data/SSL-dataset/train/' + letter + '/' + letter + str(currentframe) + '.jpg'
+            name = './data/SSL-dataset/' + folder + '/' + letter + '/' + letter + str(currentframe) + '.jpg'
             print('Creating...' + name)
 
-            # writing the extracted images
-            frame = cv2.flip(frame, -1)
+            frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
+            # Crop image. First is height
+            frame = frame[200:920, 0:720]
             frame = cv2.resize(frame, (224, 224))
 
             cv2.imwrite(name, frame)
 
-            counter += 15  # Capture every 15th frame (if 30fps, every 0.5 seconds of clip
-            videoStream.set(1, counter)
+            # counter += 15  # Capture every 15th frame (if 30fps, every 0.5 seconds of clip
+            # videoStream.set(1, counter)
 
             currentframe += 1
 
@@ -61,3 +66,4 @@ def createImagesFromClip(letter):
 
 for letter in categories:
     createImagesFromClip(letter)
+    print('Done with', letter)
