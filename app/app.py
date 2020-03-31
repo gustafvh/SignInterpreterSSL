6bbf3e0cf6beda8b3d7720a2f5bea5da4c492a87
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2020-03-31 14:30:27 +0200

    addded app structure

---
+++ app/app.py
@@ -0,0 +1,22 @@
+from math import expm1
+
+import pandas as pd
+from flask import Flask, jsonify, request
+from tensorflow import keras
+from Sign_Language_ML_Interpreter import testThisThing
+
+app = Flask(__name__)
+
+def getModel():
+    model = keras.models.load_model("../output/model-weights-best-96.hdf5")
+    return model.summary()
+
+# curl -d '{first: 566}' -H "Content-Type: application/json" -X POST http://localhost:5000
+@app.route("/", methods=["POST"])
+def index():
+    modelInfo = getModel()
+    return jsonify(modelInfo)
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    return jsonify("predict")
