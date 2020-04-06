f4b108fea650d8c871508df0bcd94f8e5cafd3e3
Gustaf Halvardsson <gustaf.halvardsson@gmail.com>
2020-04-06 13:57:10 +0200

    rewrite root structure for app

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.scss";
+import Header from './components/Header/Header.js';
+import Footer from './components/Footer/Footer.js';
+
+import "normalize.css/normalize.css"; //NP, Reset all browsers default things
+import React, {Component} from 'react'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentStep: 1
+    };
+  };
+
+  render() {
+  return (
+    <div>
+      <div className="app__container">
+        <Header/>
+        <p> Hello </p>
+        <Footer/>
+      </div>
+    </div>
+  );
+}
+}
+
