1e3f114465add86aa93a7865525607cbafb788e3
Johanna Peterson <johanna@Johannas-MBP-2.lan>
2020-04-07 15:13:24 +0200

    changed column alignment

--- src/components/UploadImage/ImageWithSettings/SecondColumn.js
+++ src/components/UploadImage/ImageWithSettings/SecondColumn.js
@@ -6,23 +6,14 @@ import "./SecondColumn.scss";
 
 export default class SecondColumn extends Component {
 
-    renderSimilairImages = (arrayWithUrls) => {
-        return (
-        <div className="rendered-images__container">
-                {arrayWithUrls.map((singleImageUrl) => 
-                    <img className="rendered-images__single-image" onError={e => e.target.style.display = 'none'} src={singleImageUrl} alt={singleImageUrl}/>
-                    )}
-            </div> );
-    }
-
     render() {
         return (
             <div className="second-column">
             
             {this.props.gettingAPIResponse 
-                ? <div className="loading-icon__container"><div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>
+                ? <div className="loading-icon__container" style={{paddingRight: "130px"}}><div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div></div>
             : this.props.googleResponse === undefined 
-            ? <div style={{width: "400px"}}>
+            ? <div style={{width: "400px", marginLeft: "40px"}}>
                 <p style={{textAlign: "center"}}>Press the button and we will analyze which <span style={{fontWeight: "bold"}}>sign you have given us</span> </p>
                     <div className="next-step__container">
                 <button onClick={this.props.submitToCloudVisionAPI} className="next-step__button cta-button__hover"> Identify Sign <img height="20px" style={{marginLeft: "10px"}} alt="upload icon" src={smileyIcon}/></button>
@@ -30,10 +21,8 @@ export default class SecondColumn extends Component {
             </div> 
             : <div> 
             <h4>Your sign is: </h4>
-            <p>We labeled the image as <span style={{fontWeight: "bold"}}>{this.props.imageLabels[0]}</span>.</p>
-            {this.renderSimilairImages(this.props.similairImagesUrls)}
-            <p className="small-information-text">Scroll down for more images.</p>
-            <div onClick={ () => window.location.reload()} style={{display: "flex", justifyContent:"center"}}><button className="restart__button">Do it again! <img style={{marginLeft: "10px"}} height="20px" alt="upload icon" src={resetIcon}/> </button></div>
+            
+            <div onClick={ () => window.location.reload()} style={{display: "flex", justifyContent:"center"}}><button className="restart__button">Upload another image! <img style={{marginLeft: "10px"}} height="20px" alt="upload icon" src={resetIcon}/> </button></div>
                 </div>}
             </div>
         )
