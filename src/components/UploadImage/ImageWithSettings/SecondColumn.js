5e2cb49fd2e5094b2e892ce77a2ae1b51ac4abef
Johanna Peterson <johanna@Johannas-MBP-2.lan>
2020-04-09 09:15:26 +0200

    removed text to speech code

--- src/components/UploadImage/ImageWithSettings/SecondColumn.js
+++ src/components/UploadImage/ImageWithSettings/SecondColumn.js
@@ -3,44 +3,9 @@ import "../LoadingIcon.scss";
 import smileyIcon from "../../../assets/icons/smiley-icon.png";
 import resetIcon from "../../../assets/icons/reset-icon.png";
 import "./SecondColumn.scss";
-//import GOOGLE_APPLICATION_CREDENTIALS from "../../../config/SSL-interpreter-bafad22fc797.json"
-
-
+//import Environment from "../../../config/environment";
 export default class SecondColumn extends Component {
-
-    //  toAudio = () => {
-    //      const textToSpeech = require('@google-cloud/text-to-speech');
-    //      const fs = require('fs');
-    //      const util = require('util');
-
-    //      const client = new textToSpeech.TextToSpeechClient();
-
-    //      async function quickStart() {
-    //         // The text to synthesize
-    //         const text = 'hello, world!';
-          
-    //         // Construct the request
-    //         const request = {
-    //           input: {text: text},
-    //           // Select the language and SSML voice gender (optional)
-    //           voice: {languageCode: 'sv-SE', ssmlGender: 'NEUTRAL'},
-    //           // select the type of audio encoding
-    //           audioConfig: {audioEncoding: 'MP3'},
-    //         };
-          
-    //         // Performs the text-to-speech request
-    //         const [response] = await client.synthesizeSpeech(request);
-    //         // Write the binary audio content to a local file
-    //         const writeFile = util.promisify(fs.writeFile);
-    //         await writeFile('output.mp3', response.audioContent, 'binary');
-    //         console.log('Audio content written to file: output.mp3');
-    //       }
-    //       quickStart();
-    //      return(5)
-    //  }
-      
     
-
     render() {
         return (
             <div className="second-column">
@@ -55,11 +20,45 @@ export default class SecondColumn extends Component {
             </div> 
             : <div> 
             <h4>The letter you signed is: </h4> 
-            <p>Listen to the corresponding verbal letter here: </p>
-            {/* {this.toAudio()}  */}
+            {/* <p>Listen to the corresponding verbal letter here: </p> */}
+             {/* {this.toAudio()}  */}
             <div onClick={ () => window.location.reload()} style={{display: "flex", justifyContent:"center"}}><button className="restart__button">Upload another image! <img style={{marginLeft: "10px"}} height="20px" alt="upload icon" src={resetIcon}/> </button></div>
                 </div>}
             </div>
         )
     }
 }
+
+//  toAudio = () => {
+    //      const textToSpeech = require('@google-cloud/text-to-speech');
+    //      const fs = require('fs');
+    //      const util = require('util');
+    //      const path = require('path');
+
+    //      const options = {
+    //         keyFilename: path.join('../../../config/SSL-interpreter-bafad22fc797.json')
+    //     }
+    //     async function quickStart() {
+            
+    //         const client = new textToSpeech.TextToSpeechClient(options);
+            
+    //         // The text to synthesize
+    //         const text = 'hello, world!';
+    //         // Construct the request
+    //         const request = {
+    //           input: {text: text},
+    //           // Select the language and SSML voice gender (optional)
+    //           voice: {languageCode: 'sv-SE', ssmlGender: 'NEUTRAL'},
+    //           // select the type of audio encoding
+    //           audioConfig: {audioEncoding: 'MP3'},
+    //         };
+    //         // Performs the text-to-speech request
+    //         const [response] = await client.synthesizeSpeech(request);
+    //         // Write the binary audio content to a local file
+    //         const writeFile = util.promisify(fs.writeFile);
+    //         await writeFile('output.mp3', response.audioContent, 'binary');
+    //         console.log('Audio content written to file: output.mp3');
+    //       }
+    //       quickStart();
+    //      return(5)
+    //  }
